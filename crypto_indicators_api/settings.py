4df69811b1571f6d15b150aa5ba8f54300d33aa5
frez_freedom <frez1374@gmail.com>
2021-11-18 04:05:54 +0330

    Config rest_framework

--- crypto_indicators_api/settings.py
+++ crypto_indicators_api/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'indicators'
+    'rest_framework',
+    'indicators',
 ]
 
 MIDDLEWARE = [
